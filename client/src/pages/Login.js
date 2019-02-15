1d10fe8ad52e26b606d1494780954035a73bd799
divini <jeffrdaquioag@gmail.com>
2019-02-14 21:11:05 -0800

    .

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -77,23 +77,18 @@ class Login extends Component {
     render() {
         return (
 
-<<<<<<< HEAD
             <div className="loginDiv">
                 <Button onClick={this.handleShowLogin} disabled={this.state.authenticated} className="loginBtn btn btn-warning">Login</Button>
                 <Button onClick={this.handleShowSignUp} className="signUpBtn btn btn-warning">Register</Button>
                 <Button onClick={this.handleLogout} disabled={!this.state.authenticated} className="logoutBtn btn btn-warning">Logout</Button>
-=======
-            <>
-                <Button onClick={this.handleShowLogin} disabled={this.state.authenticated} id="loginBtn">Login</Button>
-                <Button onClick={this.handleShowSignUp} id="signUpBtn">Register</Button>
-                <Button onClick={this.handleLogout} disabled={!this.state.authenticated}>Logout</Button>
+
+
                 <div>
                     <Button onClick={this.handleAddSample} disabled={!this.state.authenticated} className = 'bg-success'>Add sample to userList</Button>
                 </div>
                 <div>
                     <Button onClick={this.handlePopulate} disabled={!this.state.authenticated} className = 'bg-info'>Populate</Button>
                 </div>
->>>>>>> a95aacd38ce1caae6b5fbf65a102c85919412594
                 {/* <Button onClick = {this.handleGet} id="getBtn">GET igdb</Button> */}
                 <SignUpContainer show={this.state.signUpShow} hide={this.handleCloseSignUp.bind(this)} />
                 <LoginContainer show={this.state.loginShow} hide={this.handleCloseLogin.bind(this)}
