b2712282adeeda137cad4054382f883b1bca6017
gaoqi7 <gaoqi7@gmail.com>
2019-02-18 13:22:54 -0800

    change home to class

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css"
 import Header from "../components/header/header";
 // import List from "../components/list/list";
 import Logo from "../components/logo/logo";
 import Carousel from "../components/carousel/carousel";
 import Sidebar from "../components/sidebar/sidebar";
+import API from '../util/API'
+// function home() {
+class home extends Component {
+  state = { userInfo: "" }
 
-function home() {
-  return (
-    <div className="wrapper">
-      <Sidebar />
-      <div id="content">
-        <Header />
-        <br></br>
-        <Logo />
-        <Carousel />
-        {/* <List /> */}
+  getUserName = (id) => {
+    API.getUserName(id)
+      .then(data => {
+        this.setState({ userInfo: data.data[0].name });
+        console.log(this.state.userInfo)
+      })
+      .catch(err => console.log(err))
+  }
+
+  componentDidMount() {
+    let d = localStorage.getItem('id')
+    if (d) {
+      this.getUserName({ id: d })
+    }
+  }
+
+
+
+
+  handleLogout = () => { console.log("logout") }
+  getGameList = () => { console.log("placeholder") }
+  render() {
+    return (
+      <div className="wrapper">
+        <Sidebar />
+        <div id="content">
+          <Header
+            handleLogout={this.handleLogout}
+            retrieveList={this.getGameList}
+          // userName={this.state.userInfo}
+          />
+          <br></br>
+          <Logo />
+          <Carousel />
+          {/* <List /> */}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default home;
